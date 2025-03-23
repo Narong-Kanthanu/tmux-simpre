f8ffc4d355188d273b3f2397888f60638912d119
Narong Kanthanu <narong.okan@gmail.com>
2025-03-23 15:54:49 +0700

    (chore): Change copy file to symlink file

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 install_config() {
   local -r source_file=$(pwd)/.tmux.conf
-  local -r destination_file=$HOME/
-  cp -r "$source_file" "$destination_file"
+  local -r destination_file=$HOME/.tmux.conf
+  ln -s "$source_file" "$destination_file"
   echo "Install success, please press Ctrl+r in tmux mode to reload config"
 }
 
