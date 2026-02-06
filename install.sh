e705707319e9b51140e5150f46390c5a7ef15abc
Narong Kanthanu <narong.okan@gmail.com>
2026-02-06 15:22:26 +0700

    (chore): Update message when install tmux config

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ install_config() {
   local -r source_file=$(pwd)/.tmux.conf
   local -r destination_file=$HOME/.tmux.conf
   ln -s "$source_file" "$destination_file"
-  echo "Install success, please press Ctrl+r in tmux mode to reload config"
+  echo "Install success, please press Ctrl+b+r in tmux mode to reload config"
 }
 
 # run install
